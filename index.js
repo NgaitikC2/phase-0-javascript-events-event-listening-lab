730934a719524efe8fc8e84f17d8127be593d5ec
Ngai Tik Chung <NgaitikK.Chung94@gmail.com>
2023-08-21 23:04:00 -0400

    index.html

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-function addingEventListener() {
-}
+    function clickHandler() {
+      alert('Button clicked!');
+    }
+  
+    function addingEventListener() {
+      const button = document.getElementById('button');
+  
+      if (button) {
+        button.addEventListener('click', clickHandler);
+      }
+    }
+  
+
+    addingEventListener();
+
+  
